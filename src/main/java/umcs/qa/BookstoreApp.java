1e14ea776714d56d1b3136f5b18b2ed2596d8ee9
Dominika Jusinska <dominika.jusinska@gmail.com>
2019-05-29 15:20:50 +0200

    Project structure

---
+++ src/main/java/umcs/qa/BookstoreApp.java
@@ -0,0 +1,13 @@
+package umcs.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(BookstoreApp.class, args);
+    }
+}
